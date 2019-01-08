9a67eb37210a748f291979fbeb14c093f1d5a006
ddiu8081 <ddiu8081@gmail.com>
2019-01-08 20:46:52 +0800

    add multi-lang

--- assets/js/oreo.js
+++ assets/js/oreo.js
@@ -1,12 +1,120 @@
 var images = {}; // Cache Images
+const messages = {
+    zh_CN: {
+        name: "中",
+        basic: {
+            o: "奥",
+            r: "利",
+            and: "与"
+        },
+        input: {
+            meta: "我想要：",
+            generate: "生成",
+            btn: {
+                o: "+奥",
+                r: "+利",
+                and: "+与"
+            },
+        },
+        output: {
+            meta: "這是你的",
+            save: "保存图片",
+            back: "返回"
+        }
+    },
+    en: {
+        name: "En",
+        basic: {
+            o: "O | o",
+            r: "Re | re",
+            and: "-"
+        },
+        input: {
+            meta: "I'd like:",
+            generate: "Generate",
+            btn: {
+                o: "O",
+                r: "R",
+                and: "and"
+            },
+        },
+        output: {
+            meta: "Here's your",
+            save: "Save Image",
+            back: "Back"
+        }
+    },
+    ja: {
+        name: "日",
+        basic: {
+            o: "オ",
+            r: "レ",
+            and: "と"
+        },
+        input: {
+            meta: "私は...したい",
+            generate: "実行",
+            btn: {
+                o: "+オ",
+                r: "+レ",
+                and: "+と"
+            },
+        },
+        output: {
+            meta: "結果",
+            save: "画像を保存する",
+            back: "元に戻す"
+        }
+    }
+}
+
+// Set Language
+var lang;
+if (window.localStorage.getItem('lang')) {
+    lang = window.localStorage.getItem('lang');
+} else {
+    localStorage.lang = "zh_CN";
+    lang = "zh_CN";
+}
+
+const i18n = new VueI18n({
+    locale: lang, // set locale
+    messages, // set locale messages
+})
+
 var app = new Vue({
     el: '#app',
+    i18n,
     data: {
+        lang: messages,
         output: false,
         loading: true,
-        oreoStr: "",
         oreoArr: [],
         imgUrl: ""
+    },
+    computed: {
+        oreoStr: function () {
+            var str = "";
+            for (let index = 0; index < this.oreoArr.length; index++) {
+                const item = this.oreoArr[index];
+                console.log(index,item)
+                switch (item) {
+                    case "O":
+                    case "Ob":
+                        str += this.$i18n.tc("basic.o", index + 1);
+                        break;
+                    case "R":
+                        str += this.$i18n.tc("basic.r", index + 1);
+                        break;
+                    case "-":
+                        str += this.$i18n.t("basic.and", index);
+                        break;
+                    default:
+                        break;
+                }
+            }
+            return str;
+        }
     },
     created: function () {
         var that = this;
@@ -22,6 +130,10 @@ var app = new Vue({
         });
     },
     methods: {
+        changeLang: function (lang) {
+            localStorage.lang = lang;
+            this.$i18n.locale = lang;
+        },
         loadImages: function (sources, callback) {
             var cacheImages = {};
             var index = 0;
@@ -42,7 +154,7 @@ var app = new Vue({
             console.log(str);
             switch (str) {
                 case 'o':
-                    this.oreoStr += "奥";
+                    // this.oreoStr += this.$i18n.t("basic.o");
                     if (this.oreoArr.length == 0) {
                         this.oreoArr.push("O");
                     } else {
@@ -50,19 +162,19 @@ var app = new Vue({
                     }
                     break;
                 case 'r':
-                    this.oreoStr += "利";
+                    // this.oreoStr += this.$i18n.t("basic.r");
                     this.oreoArr.push("R");
                     break;
                 case '-':
                     if (this.oreoArr.length > 0 && this.oreoArr[this.oreoArr.length - 1] != '-') {
-                        this.oreoStr += "与";
+                        // this.oreoStr += this.$i18n.t("basic.and");
                         this.oreoArr.push("-");
                     }
                     break;
                 case '-1':
                     if (this.oreoArr.length > 0) {
                         this.oreoArr.pop();
-                        this.oreoStr = this.oreoStr.substr(0, this.oreoStr.length - 1);
+                        // this.oreoStr = this.oreoStr.substr(0, this.oreoStr.length - 1);
                     }
                     break;
                 default:
@@ -75,7 +187,7 @@ var app = new Vue({
             this.output = true;
             var oreoArr = this.oreoArr;
             var drawArr = [];
-            
+
             // Delete '-' at the end
             if (oreoArr[oreoArr.length - 1] == "-") {
                 oreoArr.pop();
