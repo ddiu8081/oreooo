44bca1a1cface658f3f082ee91a97c6d04054c7f
ddiu8081 <ddiu8081@gmail.com>
2022-07-24 01:04:20 +0800

    feat: animation

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,27 @@
 <script setup lang="ts">
   import Home from './pages/Home.vue'
   import Result from './pages/Result.vue'
+  import Loading from './components/Loading.vue'
+import { onMounted } from 'vue';
 
   let oreoList = $ref<OreoKey[]>([])
+  let loading = $ref(true)
+  let animating = $ref(true)
+
+  onMounted(() => {
+    setTimeout(() => {
+      loading = false
+    }, 1000)
+  })
 
   const handleSubmit = (newList: OreoKey[]) => {
+    loading = true
+    console.log('loading', loading)
+    setTimeout(() => {
+      loading = false
+      console.log('loading', loading)
+    }, 1000)
+
     oreoList = [...newList]
   }
 </script>
@@ -15,8 +32,19 @@
     min-h-screen w-screen
     bg="#caad9f" p-12
   >
-    <Home v-if="!oreoList.length" @submit="handleSubmit" />
-    <Result v-else :oreoList="oreoList" @back="oreoList=[]" />
+    <div v-show="animating" absolute inset-0 flex items-center justify-center>
+      <Transition
+        appear name="bounce"
+        @before-enter="animating = true"
+        @after-leave="animating = false"
+      >
+        <Loading v-show="loading" />
+      </Transition>
+    </div>
+    <template v-if="!loading && !animating">
+      <Home v-if="!oreoList.length" @submit="handleSubmit" />
+      <Result v-else :oreoList="oreoList" @back="oreoList=[]" />
+    </template>
   </div>
 </template>
 
@@ -32,5 +60,23 @@ body {
     font-family: "Seto";
     src: url("./assets/fonts/Seto.woff") format("woff"),
         url("./assets/fonts/Seto.ttf") format("truetype");
+}
+
+.bounce-enter-active {
+  animation: bounce-in 0.5s;
+}
+.bounce-leave-active {
+  animation: bounce-in 0.5s reverse;
+}
+@keyframes bounce-in {
+  0% {
+    transform: scale(0);
+  }
+  50% {
+    transform: scale(1.1);
+  }
+  100% {
+    transform: scale(1);
+  }
 }
 </style>
